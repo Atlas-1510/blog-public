882ac31a4b5df643712a354fa7df8a8bd44c1244
Jason Aravanis <j.aravanis@icloud.com>
2021-11-18 10:28:47 +1100

    Set up tailwind

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,11 +8,11 @@ function Article({ article }) {
   });
 
   return (
-    <div key={article._id}>
+    <div key={article._id} className="text-red-500">
       <h2>{article.title}</h2>
       <p>{article.description}</p>
       <p>{article.author.username}</p>
-      <p>{formattedDate}</p>
+      <p className="text-green-500">{formattedDate}</p>
     </div>
   );
 }
