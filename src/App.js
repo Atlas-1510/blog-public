e5e331a3204801a91ee4ea65af24da7ce2f745b3
Jason Aravanis <j.aravanis@icloud.com>
2021-11-18 16:10:03 +1100

    Limit App width to 360px, to show correctly on very narrow screens

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="font-roboto flex flex-col items-center min-h-screen">
+    <div className="font-roboto flex flex-col items-center min-h-screen min-w-[360px]">
       <Header />
       <CardsContainer />
       <Footer />
