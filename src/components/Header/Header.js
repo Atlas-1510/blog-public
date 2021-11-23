8918fd09b9d127f6bc433627abc3a263d3cddbba
Jason Aravanis <j.aravanis@icloud.com>
2021-11-23 14:53:10 +1100

    Add responsive 'Home' nav link to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 
@@ -14,15 +14,15 @@ function Header({ clearValue }) {
   if (!user) {
     return (
       <nav className="w-full flex justify-end p-4">
-        <Link to="/" className="nav-link">
+        <NavLink to="/" className="nav-link">
           Home
-        </Link>
-        <Link to="/signin" className="nav-link">
+        </NavLink>
+        <NavLink to="/signin" className="nav-link">
           Sign in
-        </Link>
-        <Link to="/signup" className="highlight-button">
+        </NavLink>
+        <NavLink to="/signup" className="highlight-button">
           Sign up
-        </Link>
+        </NavLink>
       </nav>
     );
   } else {
@@ -30,9 +30,15 @@ function Header({ clearValue }) {
       <nav className="w-full flex justify-between items-center p-4">
         <h1 className=" text-xl text-secondary">Hello {`${user.username}`}</h1>
         <div className="flex items-center">
-          <Link to="/" className="nav-link">
+          <NavLink
+            end
+            to="/articles"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : "nav-link"
+            }
+          >
             Home
-          </Link>
+          </NavLink>
           <button
             onClick={(e) => handleSignOut(e)}
             className="nav-link bg-highlight text-white hover:text-white hover:bg-indigo-700"
